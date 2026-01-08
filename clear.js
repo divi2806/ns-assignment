fe7a1ddabd87632e358b41d870c3f50cd67a2799
NinjasPyajamas <divyansh242805@gmail.com>
2026-01-08 15:25:00 +0800

    Removed testing files

--- clear.js
+++
@@ -1,6 +0,0 @@
-import postgres from 'postgres';
-const sql = postgres("postgresql://neondb_owner:npg_vTjaBe43XOgq@ep-shy-grass-ady7hd2d-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require", { ssl: 'require', max: 1 });
-const r = await sql`DELETE FROM activity_cache`;
-console.log('✅ Cleared', r.length, 'cache entries');
-await sql.end();
-
